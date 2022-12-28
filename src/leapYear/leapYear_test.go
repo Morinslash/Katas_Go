ea5ca89962db3e6f6490272af34a881a1982a7dd
Morinslash <sebastian.elf.kulinski@gmail.com>
2022-12-28 15:35:33 +0100

    solved LeapYear

--- src/leapYear/leapYear_test.go
+++ src/leapYear/leapYear_test.go
@@ -34,14 +34,8 @@ func TestLeapYear(t *testing.T) {
 }
 
 func isLeap(year int) bool {
-	if year%400 == 0 {
-		return true
-	}
 	if year%100 == 0 && year%400 != 0 {
 		return false
 	}
-	if year%4 == 0 {
-		return true
-	}
-	return false
+	return year%4 == 0
 }
